772abde808d069eb63179efb9ef770a8868ee1be
Dennis Wong <denniswong0106@gmail.com>
2021-03-10 04:53:27 +0000

    added so that my snake, when I connect, has a name -_-

--- client.js
+++ client.js
@@ -10,9 +10,16 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  conn.on('connect', data => {
+    console.log('sucessfully connected to game server')
+  });
+  conn.on('connect', () => {
+    conn.write('Name: -_-');
+  });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
+
   return conn;
 }
 
