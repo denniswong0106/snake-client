476709e9c61923b8aa6b78ddc1d152a2556221bc
Dennis Wong <denniswong0106@gmail.com>
2021-03-10 05:12:48 +0000

    added handleUserInput, that allows us to back out of the game with ctrl + c

--- play.js
+++ play.js
@@ -2,3 +2,21 @@ const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
 
+handleUserInput = key => {
+  if (key === '\u0003') {
+  console.log('Wow you quit already?')
+  process.exit();
+  }
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+setupInput();
+
